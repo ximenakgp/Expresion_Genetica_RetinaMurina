40c21651d820929391e28abd1c4bc775eaad315b
Ednakrz <ednakrz@lcg.unam.mx>
2024-11-29 22:10:25 -0600

    Comentarios

--- src/entrez_busqueda.py
+++ src/entrez_busqueda.py
@@ -19,13 +19,41 @@ CATEGORY:
         Proyecto de Biopython: Expresion_Genetica_RetinaMurina
 
 USAGE:
-        % python analisis_regulacion.py -i <ruta al archivo> -o <ruta a donde se guardaran los resultados> 
-          -c <nombre de la columna> -r <tipo de regulación>
+        % python entrez_busqueda.py -i <ruta al archivo> -o <ruta a d
+        onde se guardaran los resultados> -c <cantidad de genes a bus
+        car> -s <tipo de regulación (maxima o minima)> -e <email por 
+        elque se realizara la busqueda> -d <Base de datos que quieres
+        consultar>
 
-        % gen_ontology_ora.py [-h] -i INPUT -o OUTPUT [-g ORGANISMO]
-        EXAMPLES:       
+        EXAMPLES:
+
+        % python entrez_busqueda.py -i ../results/genes_regulacion_negativa.txt -o ../results/genes_entrez.txt -c 10 -s menor -e ednakrz@lcg.unam.mx -d gene
+
+        % python entrez_busqueda.py -i ../results/genes_regulacion_negativa.txt -o ../results/genes_entrez.txt -c 10 -s mayor -e ednakrz@lcg.unam.mx -d gene
 
 PARAMETERS:
+
+%  python entrez_busqueda.py --help
+
+Busqueda de los genes con menor o mayor expresión diferencial
+
+options:
+  -h, --help            show this help message and exit
+  -i INPUT, --input INPUT
+                        Ruta del archivo de entrada (TSV)
+  -o OUTPUT, --output OUTPUT
+                        Ruta del archivo de salida
+  -c CANTIDAD, --cantidad CANTIDAD
+                        Cantidad de genes ha buscar
+  -s {positiva,negativa}, --regulacion {mayor,menor}
+                        Tipo de expresion, de mayor o menor significancia
+  -e EMAIL, --email EMAIL 
+                        Correo por el cual se conectara a Entrez 
+  -d BASEDATOS, --basedatos BASEDATOS 
+                        Base de datos en la que que quieres buscar tus  genes
+                        expresados con mayor o menor significancia
+
+    
 
 '''
 
@@ -43,8 +71,11 @@ import pandas as pd
 
 
 def main():
+    #Leer el archivo de entrada 
     args = obtener_argumentos()
     df = leer_archivo(args.input)
+    
+    # Filtrar los genes 
     if df is not None:
         genes_filtrados = filtrar_genes(df,args.cantidad, args.significancia)
         if genes_filtrados is not None:
@@ -54,7 +85,7 @@ def main():
             regulacion = args.significancia
             output_file = args.output.replace(".txt", f"_{regulacion}.txt")
             guardar_genes(busqueda, output_file)
-            
+
 def obtener_argumentos():
     """
     Obtiene argumentos desde la línea de comandos
@@ -90,7 +121,7 @@ def filtrar_genes(df,cantidad, regulacion="mayor"):
 
     Args:
         df (DataFrame): DataFrame con los datos de expresión.
-        columna (str): Columna para aplicar el filtro (por defecto, "pvalue").
+        cantidad (int): Cantidad de genes a obtener.
         regulacion (str): Tipo de regulación ("mayor" o "menor").
 
     Returns:
@@ -121,14 +152,24 @@ def filtrar_genes(df,cantidad, regulacion="mayor"):
     return genes_filtrados
 
 def busqueda_entrez (email, genes_filtrados, database): 
-   
-    # Correo
+    """
+    Buscar los genes seleccionados en a base de datos querida a partir de Entrez
+    Args:
+        email (str): Direccion de correo electronico para acceder a Entrez. 
+        genes_filtrados (list): Lista de genes filtrados segun el tipo significancia.
+        database (str): Base de datos donde se buscaran los genes filtrados.
+
+    Returns:
+        DataFrame: Genes filtrados según el criterio.
+
+    """
+    # Correo para accesar a Entrez
     Entrez.email = email
     # Lista para almacenar los resultados
     resultados = [] 
     for gene_filtrado in genes_filtrados:
          try: 
-                 # Realiza la búsqueda en la base de datos Nucleotide
+                 # Realiza la búsqueda en la base de datos 
                  handle = Entrez.efetch(db=database, id=gene_filtrado, rettype="gb", retmode="text")
                  data = handle.read()
                  handle.close()
